bd0ceede4dd6745797375fe66a18095ec4277cd5
Sushmitha V <vsushmitha10@gmail.com>
2024-06-16 13:38:44 +0530

    Home now reads videos from Database.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Mainpage from "./Mainpage";
 import "remixicon/fonts/remixicon.css";
 import Cookies from "js-cookie";
-import data from "../resources/all.json";
+
 const Home = () => {
-  const videosArray = data.videos;
+  const port = 4444;
+  const thisPage = "Home";
+  const [videosArray, setVideosArray] = useState([]);
+  const [loading, setLoading] = useState("Loading");
+  const [myProps, setProps] = useState({ videos: [], src: thisPage });
 
   useEffect(() => {
     const jwtToken = Cookies.get("jwt_token");
@@ -12,16 +16,85 @@ const Home = () => {
       Cookies.remove("jwt_token");
       window.location.href = "/login";
     }
+    if (loading === "Loading") {
+      console.log(`${thisPage} : Page is ${loading}`);
+      const resp = fetchVideos();
+      if (resp != 200) {
+        console.log(`${thisPage} : Page is already ${loading}`);
+        return;
+      }
+      setProps({ videos: videosArray, src: thisPage });
+      console.log(`${thisPage} : Fetched ${videosArray.length} videos`);
+    } else if (loading === "Fetched" && myProps.videos.length === 0) {
+      setProps({ videos: videosArray, src: thisPage });
+      setLoading("Displayed");
+    } else if (loading === "Failed") {
+      setProps({ videos: videosArray, src: thisPage });
+    } else {
+      console.log(`${thisPage} : Page is already ${loading}`);
+    }
   });
 
-  const props = {
-    videos: videosArray,
-    src: "Home",
+  const fetchVideos = async () => {
+    if (loading === "Fetching" || loading === "Failed") {
+      return 201;
+    }
+
+    setLoading("Fetching");
+    const jwtToken = Cookies.get("jwt_token");
+    if (jwtToken === undefined) {
+      setLoading("Failed");
+      Cookies.remove("jwt_token");
+      window.location.href = "/login";
+    }
+    try {
+      const response = await fetch(
+        `http://localhost:${port}/get-video-details`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: jwtToken,
+          },
+        }
+      );
+
+      if (response.ok) {
+        const filterdData = await response.json();
+        setVideosArray(filterdData);
+        console.log(filterdData);
+        setLoading("Fetched");
+        return response.status;
+      } else {
+        setLoading(response.message);
+        console.log(
+          `${thisPage} : Error while fetching videos. ${response.error}`
+        );
+        setLoading("Failed");
+        if (response.status === 403 || response.status === 401) {
+          Cookies.remove("jwt_token");
+          window.location.href = "/login";
+        }
+        return response.status;
+      }
+    } catch (error) {
+      setLoading("Failed");
+      console.log(
+        `${thisPage} : Error while fetching videos. ${error.message}`
+      );
+    }
+    return 400;
   };
 
   return (
     <div>
-      <Mainpage props={props} />
+      {loading === "Failed" ||
+      loading === "Displayed" ||
+      myProps.videos.length > 0 ? (
+        <Mainpage props={myProps} />
+      ) : (
+        "Loading"
+      )}
     </div>
   );
 };
